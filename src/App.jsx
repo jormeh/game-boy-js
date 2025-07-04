2a9bf7f6010cfb4dcedc0b28718b62df51055064
Jordan <jormeh@pm.me>
2025-07-04 15:55:47 -0300

    Create DPad component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import Button from './components/Button'
 import PowerIcon from './components/PowerIcon'
+import DPad from './components/DPad'
 
 export default function App() {
   return (
     <>
-      <Button label="A" size="medium"/>
+      {/* <Button label="A" size="medium"/>
       <Button label="B" size="medium"/>
       <Button icon={PowerIcon} size="small"/>
       <Button size="small"/>
-      <Button size="small"/>
+      <Button size="small"/> */}
+      <DPad />
     </>
   )  
 }
