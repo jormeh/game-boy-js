c4ed9a994d368df5192b8564ed09115a9dd1407e
Jordan <jormeh@pm.me>
2025-07-07 22:46:44 -0300

    Create GameBoy component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
-import Button from './components/Button'
-import PowerIcon from './components/PowerIcon'
-import DPad from './components/DPad'
+import GameBoy from './components/GameBoy'
 
 export default function App() {
   return (
     <>
-      <Button label="A" size="medium"/>
-      <Button label="B" size="medium"/>
-      <Button icon={PowerIcon} size="small"/>
-      <Button size="small"/>
-      <Button size="small"/>
-      <DPad />
+      <GameBoy />
     </>
   )  
 }
