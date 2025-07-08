c912af841aea1a44a1c6e04eeb7bccccc504dc14
Jordan <jormeh@pm.me>
2025-07-07 22:32:00 -0300

    Create PowerLight component

---
+++ src/components/PowerLight.jsx
@@ -0,0 +1,11 @@
+import "../styles/PowerLight.css";
+
+export default function PowerLight({x = 0, y, size, isPowerOn = false}) {   
+    return (
+        <div
+            className={`light${isPowerOn ? " light--power-on" : ""}`}
+            style={{[x ? "left" : "right"]: x, top: y, width: size}}
+        >
+        </div>
+    )
+}
